8160affcedbc4f32e9ac01b4dc98caeb0ae04943
Konstantin Krumin <konstantin.krumin@gmail.com>
2023-07-19 15:29:22 -0400

    WIP conversion logic

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,15 +1,32 @@
 const SPLIT_PATTERN = /[a-zA-Z]/;
 
+const UNIT_PAIRS = [
+	{ metric: 'L', imperial: 'gal' },
+	{ metric: 'km', imperial: 'mi' },
+	{ metric: 'kg', imperial: 'lbs' }
+];
+
+const convertFractionToFloat = numStr => {
+	const [numerator, denominator] = numStr.split('/').map(Number);
+	const result = numerator / denominator;
+	return Math.round(result * 100) / 100;
+};
+
 function ConvertHandler() {
 	this.getNum = input => {
+		if (!input) return null;
+
 		const index = input.search(SPLIT_PATTERN);
+		let numStr = input.slice(0, index);
 
-		let result = input.slice(0, index);
+		if (!numStr) return 1;
 
-		return result;
+		return numStr.includes('/') ? convertFractionToFloat(numStr) : parseFloat(numStr);
 	};
 
 	this.getUnit = input => {
+		if (!input) return null;
+
 		const index = input.search(SPLIT_PATTERN);
 
 		let result = input.slice(index);
